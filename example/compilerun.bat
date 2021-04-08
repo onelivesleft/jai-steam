25dbb38ec7b661a9ddd81959d8e7f555cdc956c2
onelivesleft <iainking@gmail.com>
2021-04-08 20:32:06 +0100

    pre 1.51.0

---
+++ example/compilerun.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+
+setlocal
+call :setESC
+
+cls
+
+c:
+c:\jai\bin\jai.exe build.jai
+
+if NOT ["%errorlevel%"]==["0"] goto norun
+echo.
+echo %ESC%[32m-------------------------%ESC%[0m
+echo.
+spacewar.exe
+echo.
+echo.
+:norun
+
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
+exit /B 0
